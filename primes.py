861d76018329c8a92412ccc890a2cdf4bdfaae4b
Neel Nair <neel14nair@gmail.com>
2023-10-02 10:47:03 +0100

    working solution

--- primes.py
+++ primes.py
@@ -3,4 +3,22 @@
 
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes<=0:
+        raise ValueError("Number must be positive and greater than 0!")
+    
+    target = 0
+    start = 2
+    while target<number_of_primes:
+        count = 0
+        for i in range(2,start):
+            if start% i == 0:
+                print(f"{start} % {i}")
+                count += 1
+        if count <1:
+            target +=1
+            list.append(start)
+        start += 1
     return list
+
+print(primes(100))
